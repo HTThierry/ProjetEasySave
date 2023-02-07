ae8a3920e9b877a57069b3ed83b2219ae6bf58e3
RizSoto <sadowski.yann@gmail.com>
2023-02-07 16:26:21 +0100

    Ajout module de langue

--- ProjetOrienteObjet.ConsoleApp/Views/ConsoleAppView.cs
+++ ProjetOrienteObjet.ConsoleApp/Views/ConsoleAppView.cs
@@ -1,20 +1,28 @@
-﻿namespace ProjetEasySave.consoleApp.Views
+﻿using System.Globalization;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using ProjetEasySave.lib.ViewModels;
+
+namespace ProjetEasySave.consoleApp.Views
 {
     public class ConsoleAppView
     {
         private lib.ViewModels.ConsoleAppViewModel _viewModel = new();
+        private lib.ViewModels.langue langue = new();
 
         public void Show()
         {
-            Console.WriteLine("type de sauvegarde :");
-            Console.WriteLine("1 - Sauvegarde Complète (default)");
-            Console.WriteLine("2 - Sauvegarde Incrementielle");
+
+            Console.WriteLine(langue.LanguageData[langue.LanguageCode].ChoiceBackup.title);
+            Console.WriteLine(langue.LanguageData[langue.LanguageCode].ChoiceBackup.choice1);
+            Console.WriteLine(langue.LanguageData[langue.LanguageCode].ChoiceBackup.choice2);
             _viewModel.Choix = Console.ReadLine();
 
-            Console.WriteLine("chemin de sauvegarde initial :");
+            Console.WriteLine(langue.LanguageData[langue.LanguageCode].PathSource);
             _viewModel.PathFrom = Console.ReadLine();
 
-            Console.WriteLine("chemin de sauvegarde final :");
+            Console.WriteLine(langue.LanguageData[langue.LanguageCode].PathDest);
             _viewModel.PathTo = Console.ReadLine();
 
             _viewModel.Sauvegarde();
