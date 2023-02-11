2a34a0a7e9ba4a2e7b3ed600e6e33a88ca2b974c
HTThierry <thierry.huguet@viacesi.fr>
2023-02-11 20:17:40 +0100

    Logs Half Working

--- EasySave.lib/Services/log.cs
+++ EasySave.lib/Services/log.cs
@@ -6,25 +6,23 @@ namespace EasySave.lib.Services
 {
     public static class Log
     {
-       public static void LogFiles(string[] LogArray)
+       public static int LogFiles(string[] LogArray)
         {
             if (LogArray != null)
             {
-                Console.WriteLine(LogArray.GetType());
-                Console.ReadKey();
-                //DateTime today = DateTime.Now;
-                //string day = today.ToString("MM/dd/yyyy");
+                DateTime today = DateTime.Now;
+                string day = today.ToString("MM_dd_yyyy");
 
-                //var LogPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "EasySave.lib", "Log", "log.json");               //{day}_
+                var LogPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "EasySave.lib", "Log", $"{day}_log.json");
 
-                //string JsonLog = JsonSerializer.Serialize(LogArray);
+                string JsonLog = JsonSerializer.Serialize(LogArray);
 
-                //File.AppendAllText(LogPath, JsonLog);                                                                                           // +Environment.NewLine
+                File.AppendAllText(LogPath, JsonLog + Environment.NewLine);
 
-                //return 0;
+                return 0;
             }
-            //else
-                //return 1;
+            else
+                return 1;
         }
     }
 }
