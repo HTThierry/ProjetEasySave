9197d4953c8d5db0b2e7cda42fb33b8edacd9546
RizSoto <sadowski.yann@gmail.com>
2023-02-11 20:56:33 +0100

    Modification log

--- EasySave.lib/Services/log.cs
+++ EasySave.lib/Services/log.cs
@@ -10,12 +10,32 @@ namespace EasySave.lib.Services
         {
             if (LogArray != null)
             {
+                var logs = new List<LogEntry>();
                 DateTime today = DateTime.Now;
                 string day = today.ToString("MM_dd_yyyy");
 
                 var LogPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "EasySave.lib", "Log", $"{day}_log.json");
 
-                string JsonLog = JsonSerializer.Serialize(LogArray);
+
+                    var entry = new LogEntry
+                    {
+                        Name = LogArray[0],
+                        FileSource = LogArray[1],
+                        FileTarget = LogArray[2],
+                        destPath = LogArray[3],
+                        FileSize = int.Parse(LogArray[4]),
+                        FileTransferTime = double.Parse(LogArray[5].Replace('.', ',')),
+                        time = DateTime.Parse(LogArray[6])
+                    };
+
+                logs.Add(entry);
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                };
+                string JsonLog = JsonSerializer.Serialize(logs, options);
+
+                //string JsonLog = JsonSerializer.Serialize(LogArray, LogArray.GetType());
 
                 File.AppendAllText(LogPath, JsonLog + Environment.NewLine);
 
@@ -25,4 +45,15 @@ namespace EasySave.lib.Services
                 return 1;
         }
     }
+}
+
+public class LogEntry
+{
+    public string Name { get; set; }
+    public string FileSource { get; set; }
+    public string FileTarget { get; set; }
+    public string destPath { get; set; }
+    public int FileSize { get; set; }
+    public double FileTransferTime { get; set; }
+    public DateTime time { get; set; }
 }
