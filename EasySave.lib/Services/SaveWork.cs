460385c8e122701bf90426399753b7cb07eecb62
RizSoto <sadowski.yann@gmail.com>
2023-02-11 18:23:36 +0100

    Ajout des logs(non tester)

--- EasySave.lib/Services/SaveWork.cs
+++ EasySave.lib/Services/SaveWork.cs
@@ -1,5 +1,7 @@
 ﻿using EasySave.lib.Models;
+using System.Diagnostics;
 using System.IO;
+using System.Xml.Linq;
 
 namespace EasySave.lib.Services
 {
@@ -28,9 +30,28 @@ namespace EasySave.lib.Services
 
                     foreach (string file in files)
                     {
+                        DateTime today = DateTime.Now;
                         string fileName = Path.GetFileName(file);
                         string destFile = Path.Combine(destinationPath, fileName);
-                        File.Copy(file, destFile, true);
+                        FileInfo fileInfo = new FileInfo(file);                               //Files size
+                        long fileSize = fileInfo.Length;    
+
+                        var stopwatch = Stopwatch.StartNew();                                 //start of the stopwatch
+                        File.Copy(file, destFile, true);                                      
+                        stopwatch.Stop();                                                     // stop it
+                        double fileTransferTime = stopwatch.Elapsed.TotalSeconds;             //get the time in second
+
+                        string[] LogArray = new string[] {
+                            fileName,
+                            file,
+                            destFile,
+                            destinationPath,
+                            $"{fileSize}",
+                            $"{fileTransferTime}",
+                            today.ToString("MM/dd/yyyy hh:mm:ss")
+                        };
+                        //Append of information to the logs
+                        log.logFiles(LogArray);
                     }
 
                     return 0;
@@ -63,10 +84,16 @@ namespace EasySave.lib.Services
                         {
                             string fileName = Path.GetFileName(file);
                             string destFile = Path.Combine(destinationPath, fileName);
+                            FileInfo fileInfo = new FileInfo(file);
+                            long fileSize = fileInfo.Length;
+
+                            var stopwatch = Stopwatch.StartNew();
                             File.Copy(file, destFile, true);
+                            stopwatch.Stop();
+                            double fileTransferTime = stopwatch.Elapsed.TotalSeconds;
                         }
                     }
-                    else
+                    else // A modifier pour les logs !!!!
                     {
                         string[] files = Directory.GetFiles(sourcePath);
                         foreach (string file in files)
---
+++ EasySave.lib/Services/log.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Xml.Linq;
+
+
+namespace EasySave.lib.Services
+{
+    public class log
+    {
+       public static int logFiles(string[] LogArray)
+        {
+            if (LogArray != null)
+            {
+                DateTime today = DateTime.Now;
+                string day=today.ToString("MM/dd/yyyy");
+
+                var logPath = Path.Combine("\\Services\\log\\", $"{ day}_log.json");
+
+                string jsonLog = JsonSerializer.Serialize(LogArray);
+
+                File.AppendAllText(logPath, jsonLog + Environment.NewLine);
+
+                return 0;
+            }
+            else { return 1; }
+        }
+    }
+}
+
