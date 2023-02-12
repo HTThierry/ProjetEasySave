4d7e9e58c886423265a066b41c0d6f4f9560ccce
HTThierry <thierry.huguet@viacesi.fr>
2023-02-12 23:07:42 +0100

    Attempt to make Window Auto Refresh ~ SaveWorkModel.cs

--- EasySave.lib/Models/SaveWorkModel.cs
+++ EasySave.lib/Models/SaveWorkModel.cs
@@ -1,13 +1,58 @@
-﻿namespace EasySave.lib.Models
+﻿using System.ComponentModel;
+
+namespace EasySave.lib.Models
 {
-    public class SaveWorkModel
+    public class SaveWorkModel : INotifyPropertyChanged
     {
-        public string NameSaveWork { get; set; }
+        private string _NameSaveWork;
+        public string NameSaveWork
+        {
+            get { return _NameSaveWork; }
+            set
+            {
+                _NameSaveWork = value;
+                OnPropertyChanged("NameSaveWork");
+            }
+        }
 
-        public int TypeSaveWork { get; set; }
+        private int _TypeSaveWork;
+        public int TypeSaveWork
+        {
+            get { return _TypeSaveWork; }
+            set
+            {
+                _TypeSaveWork = value;
+                OnPropertyChanged("TypeSaveWork");
+            }
+        }
 
-        public string SourcePathSaveWork { get; set; }
+        private string _SourcePathSaveWork;
+        public string SourcePathSaveWork
+        {
+            get { return _SourcePathSaveWork; }
+            set
+            {
+                _SourcePathSaveWork = value;
+                OnPropertyChanged("SourcePathSaveWork");
+            }
+        }
 
-        public string DestinationPathSaveWork { get; set; }
+        private string _DestinationPathSaveWork;
+        public string DestinationPathSaveWork
+        {
+            get { return _DestinationPathSaveWork; }
+            set
+            {
+                _DestinationPathSaveWork = value;
+                OnPropertyChanged("DestinationPathSaveWork");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
