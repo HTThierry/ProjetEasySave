693959b45d0e2ea6fec24ed9f8a1ad08466324de
Anthony <anthony.rambaut@viacesi.fr>
2023-02-22 18:01:16 +0100

    Fonction de priorisation et de n limit fonctionnelle (manque le test multithread)

--- EasySave.lib/Services/SaveWorkService.cs
+++ EasySave.lib/Services/SaveWorkService.cs
@@ -6,7 +6,7 @@ namespace EasySave.lib.Services
 {
     public class SaveWorkService
     {
-        private bool TokenOfPriority { get; set; } = true;
+        //private bool TokenOfPriority { get; set; } = true;
         private static bool TokenOfAvailability { get; set; } = true;
 
         //public SaveWorkModel _SaveWorkModel { get; set; } = new SaveWorkModel();
@@ -55,27 +55,6 @@ namespace EasySave.lib.Services
 
                 string[] AllFiles = Directory.GetFiles(copyModel.SourcePath, "*.*", SearchOption.AllDirectories);
 
-                string[] SpecificExtensions = ConfigurationManager.AppSettings["FileToPrioritize"].Split(',');
-                long FileSizeLimit = long.Parse(ConfigurationManager.AppSettings["FileSizeLimit"]);
-
-                string[] PrioritizedFiles = AllFiles.Where(file => SpecificExtensions.Contains(Path.GetExtension(file))).ToArray();
-                string[] OtherFiles = AllFiles.Except(PrioritizedFiles).ToArray();
-
-                // Création des 4 listes
-                string[] PrioritizedBigFiles = PrioritizedFiles.Where(file => new FileInfo(file).Length <= FileSizeLimit).ToArray();
-                string[] PrioritizedSmallFiles = PrioritizedFiles.Except(PrioritizedFiles).ToArray();
-
-                string[] OtherBigFiles = OtherFiles.Where(file => new FileInfo(file).Length >= FileSizeLimit).ToArray();
-                string[] OtherSmallFiles = OtherFiles.Except(OtherBigFiles).ToArray();
-
-                // Création des 4 index
-
-                int indexA = 0;
-                int indexB = 0;
-                int indexC = 0;
-                int indexD = 0;
-
-
                 copyModel.TotalFilesToCopy = AllFiles.Length;
                 copyModel.NbFilesLeft = copyModel.TotalFilesToCopy;
 
@@ -85,8 +64,67 @@ namespace EasySave.lib.Services
                 }
                 copyModel.FilesSizeLeft = copyModel.TotalFilesSizeToCopy;
 
-                CompleteCopyListOfFiles(PrioritizedFiles, model, copyModel);
-                CompleteCopyListOfFiles(OtherFiles, model, copyModel);
+                string[] SpecificExtensions = ConfigurationManager.AppSettings["FileToPrioritize"].Split(',');
+                long FileSizeLimit = long.Parse(ConfigurationManager.AppSettings["FileSizeLimit"]);
+
+                string[] PrioritizedFiles = AllFiles.Where(file => SpecificExtensions.Contains(Path.GetExtension(file))).ToArray();
+                string[] OtherFiles = AllFiles.Except(PrioritizedFiles).ToArray();
+
+                // Création des 4 listes
+                string[] PrioritizedBigFiles = PrioritizedFiles.Where(file => new FileInfo(file).Length >= FileSizeLimit).ToArray();
+                string[] PrioritizedSmallFiles = PrioritizedFiles.Except(PrioritizedBigFiles).ToArray();
+
+                string[] OtherBigFiles = OtherFiles.Where(file => new FileInfo(file).Length >= FileSizeLimit).ToArray();
+                string[] OtherSmallFiles = OtherFiles.Except(OtherBigFiles).ToArray();
+
+
+                // Création des 4 index
+
+                int indexA = 0;
+                int indexB = 0;
+                int indexC = 0;
+                int indexD = 0;
+
+                // Création de la boucle infernale
+
+                for (int i = 0; i < AllFiles.Length; i++)
+                {
+                    if (indexA != PrioritizedBigFiles.Length || indexB != PrioritizedSmallFiles.Length)
+                    {
+                        if (TokenOfAvailability && indexA != PrioritizedBigFiles.Length)
+                        {
+                            TokenOfAvailability = false;
+                            CompleteFileCopy(PrioritizedBigFiles[indexA], model, copyModel);
+                            // Copy PrioritizedBigFiles[indexA]
+                            indexA++;
+                            TokenOfAvailability = true;
+                        }
+                        else
+                        {
+                            CompleteFileCopy(PrioritizedSmallFiles[indexB], model, copyModel);
+                            // Copy PrioritizedSmallFiles[indexB]
+                            indexB++;
+                        }
+                    }
+                    else
+                    {
+                        if (TokenOfAvailability && indexB != OtherBigFiles.Length)
+                        {
+                            TokenOfAvailability = false;
+                            CompleteFileCopy(OtherBigFiles[indexC], model, copyModel);
+                            indexC++;
+                            TokenOfAvailability = true;
+                        }
+                        else
+                        {
+                            CompleteFileCopy(OtherSmallFiles[indexD], model, copyModel);
+                            indexD++;
+                        }
+                    }
+                }
+
+                //CompleteCopyListOfFiles(PrioritizedFiles, model, copyModel);
+                //CompleteCopyListOfFiles(OtherFiles, model, copyModel);
             }
             model.ProgressStateModel.Name = model.NameSaveWork;
             model.ProgressStateModel.Time = DateTime.Now;
@@ -157,6 +195,59 @@ namespace EasySave.lib.Services
 
             ProgressStateService.ProgressStateFile();
             return 0;
+        }
+
+        private void CompleteFileCopy(string file, SaveWorkModel model, CopyModel copyModel)
+        {
+            int timeForCryp = 0;
+
+            string[] ExtensionToCrypt = ConfigurationManager.AppSettings["fileToCryp"].Split(',');
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            if (ExtensionToCrypt.Contains(Path.GetExtension(file)))
+            {
+                timeForCryp = cryptoSoft.cryptoSoftEasySave("-c", $"{file}", $"{file.Replace(copyModel.SourcePath, copyModel.DestinationPath)}");
+            }
+            else
+            {
+                File.Copy(file, file.Replace(copyModel.SourcePath, copyModel.DestinationPath), true);
+            }
+
+            stopwatch.Stop();
+            double FileTransferTime = stopwatch.Elapsed.TotalSeconds;
+
+            //pourcent = (((copyModel.TotalFilesToCopy - copyModel.NbFilesLeft) / copyModel.TotalFilesToCopy));
+
+            copyModel.Percentage = (((float)copyModel.TotalFilesToCopy - (float)copyModel.NbFilesLeft) / (float)copyModel.TotalFilesToCopy) * 100;
+            copyModel.NbFilesLeft--;
+            copyModel.FilesSizeLeft -= new FileInfo(file).Length;
+
+            LogModel logModel = new LogModel()
+            {
+                Name = Path.GetFileName(file),
+                FileSource = file,
+                FileTarget = Path.Combine(copyModel.DestinationPath, Path.GetFileName(file)),
+                DestPath = copyModel.DestinationPath,
+                TimeToCrypt = timeForCryp,
+                FileSize = new FileInfo(file).Length,
+                FileTransferTime = FileTransferTime,
+                Time = DateTime.Now,
+            };
+
+            model.ProgressStateModel.Name = model.NameSaveWork;
+            model.ProgressStateModel.Time = DateTime.Now;
+            model.ProgressStateModel.ProgressState = "Active";
+            model.ProgressStateModel.TotalFilesToCopy = copyModel.TotalFilesToCopy;
+            model.ProgressStateModel.TotalFilesSizeToCopy = copyModel.TotalFilesSizeToCopy;
+            model.ProgressStateModel.NbFilesLeft = copyModel.NbFilesLeft;
+            model.ProgressStateModel.FilesSizeLeft = copyModel.FilesSizeLeft;
+            model.ProgressStateModel.Percentage = copyModel.Percentage;
+            model.ProgressStateModel.FilePath = file;
+            model.ProgressStateModel.FileDestinationPath = Path.Combine(copyModel.DestinationPath, Path.GetFileName(file));
+
+            LogService.LogFiles(logModel);
+            ProgressStateService.ProgressStateFile();
         }
 
         private void CompleteCopyListOfFiles(string[] Files, SaveWorkModel model, CopyModel copyModel)
