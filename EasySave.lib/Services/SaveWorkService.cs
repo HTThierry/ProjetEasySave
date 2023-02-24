f3cc24796f36938aa922333b26966cc4fa5f5827
HTThierry <thierry.huguet@viacesi.fr>
2023-02-24 03:47:16 +0100

    added RunningProcess to CompleteCopy

--- EasySave.lib/Services/SaveWorkService.cs
+++ EasySave.lib/Services/SaveWorkService.cs
@@ -9,6 +9,7 @@ namespace EasySave.lib.Services
 {
     public class SaveWorkService
     {
+        public SaveWorkManager saveWorkManager = SaveWorkManager.GetInstance();
         //private bool TokenOfPriority { get; set; } = true;
         private static bool TokenOfAvailability { get; set; } = true;
 
@@ -70,7 +71,6 @@ namespace EasySave.lib.Services
 
         private int CompleteCopyFiles(SaveWorkModel model)
         {
-            
             CopyModel copyModel = new CopyModel()
             {
                 SourcePath = model.SourcePathSaveWork,
@@ -258,6 +258,11 @@ namespace EasySave.lib.Services
 
         private void CompleteFileCopy(string file, SaveWorkModel model, CopyModel copyModel)
         {
+            while(saveWorkManager.CheckRunningProcess(ConfigurationManager.AppSettings["RunningProcess"]) == true)
+            {
+                Debug.WriteLine("Logiciel Métier Actif");
+            }
+
             int timeForCryp = 0;
 
             string[] ExtensionToCrypt = ConfigurationManager.AppSettings["fileToCryp"].Split(',');
